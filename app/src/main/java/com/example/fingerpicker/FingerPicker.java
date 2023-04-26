69366bef026a71d318b7561c759f8545f71fe08a
neumeiermo84285 <neumeiermo84285@th-nuernberg.de>
2023-04-26 17:51:40 +0200

    Bugfixing
    Es besteht immer noch das Problem, dass wenn man mehrere Finger auf den Bildschirm legt und z.B. den Finger entfernt der als erstes den Screen berührt hat das Programm abstürzt (Wahrscheinlich liegt das Problem an Zeile 75 "listFingerPositions.remove(pointerId);")

--- app/src/main/java/com/example/fingerpicker/FingerPicker.java
+++ app/src/main/java/com/example/fingerpicker/FingerPicker.java
@@ -1,13 +1,14 @@
 package com.example.fingerpicker;
 
+
 import static android.content.ContentValues.TAG;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
-import android.graphics.RectF;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -17,43 +18,39 @@ import java.util.Random;
 
 public class FingerPicker extends View {
 
-    private Paint paint;
-    private Random rgen = new Random();
-    private boolean isDrawing;
+    private final Random rgen = new Random();
+    private final Paint paint = new Paint();
 
-    //private int numberOfPointers;
-    //private int []x = new int[10];
-    //private int []y = new int[10];
-    private ArrayList<PointF>listFingerPositions = new ArrayList<>();
+    private final ArrayList<PointF>listFingerPositions = new ArrayList<>();
 
 
 
     public FingerPicker(Context context) {
         super(context);
-
-        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setStyle(Paint.Style.FILL);
-        //isDrawing = false;
     }
 
     protected void onDraw(Canvas canvas) {
 
+
         for(int i=0; i< listFingerPositions.size(); i++){
-            PointF fingerPosition3 = listFingerPositions.get(i);
-            Paint paint = new Paint();
+            PointF fingerPosition = listFingerPositions.get(i);
             paint.setColor(Color.RED);
-            canvas.drawCircle(fingerPosition3.x,fingerPosition3.y, 200, paint);
+            canvas.drawCircle(fingerPosition.x,fingerPosition.y, 200f, paint);
         }
+
         invalidate();
     }
 
 
+    @SuppressLint("ClickableViewAccessibility")
     public boolean onTouchEvent(MotionEvent event) {
 
+        //Index der Arrays in der die Fingerposition (x und y) gespeichert sind
         int pointerIndex = event.getActionIndex();
+        //Eindeutige Kennung die einem Finger zugeordnet wird
         int pointerId = event.getPointerId(pointerIndex);
 
-        switch (event.getAction()) {
+        switch (event.getActionMasked()) {
             case MotionEvent.ACTION_DOWN:
             case MotionEvent.ACTION_POINTER_DOWN:
                 PointF fingerPosition = new PointF(event.getX(pointerIndex), event.getY(pointerIndex));
@@ -63,17 +60,22 @@ public class FingerPicker extends View {
             case MotionEvent.ACTION_MOVE:
                 for(int i=0; i<event.getPointerCount(); i++){
                     int id = event.getPointerId(i);
-                    PointF fingerPosition2 = listFingerPositions.get(id);
-                    fingerPosition2.x = event.getX(i);
-                    fingerPosition2.y = event.getY(i);
+                    fingerPosition = listFingerPositions.get(id);
+                    fingerPosition.x = event.getX(i);
+                    fingerPosition.y = event.getY(i);
                 }
                 //Log.i(TAG, "ACTION_MOVE");
                 invalidate();
                 break;
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_POINTER_UP:
+                Log.i(TAG, "ACTION_UP");
+
+                Log.i(TAG, "REMOVE: " + pointerId);
                 listFingerPositions.remove(pointerId);
-                //Log.i(TAG, "ACTION_UP");
+
+
+
                 invalidate();
                 break;
         }
