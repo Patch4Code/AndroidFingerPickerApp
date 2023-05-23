debc74d05b6fa2fc7e0dc682830519e7aef7ea0b
neumeiermo84285 <neumeiermo84285@th-nuernberg.de>
2023-05-23 12:27:58 +0200

    Zufallsfarben und vorläufige Gewinnerauswahl

--- app/src/main/java/com/example/fingerpicker/FingerPicker.java
+++ app/src/main/java/com/example/fingerpicker/FingerPicker.java
@@ -1,74 +1,147 @@
 package com.example.fingerpicker;
 
 
-import static android.content.ContentValues.TAG;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
+//import android.util.Log;
+import android.os.Handler;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
+
 import java.util.HashMap;
+import java.util.Random;
 
 public class FingerPicker extends View {
-
     private final Paint paint = new Paint();
-    private final HashMap<Integer, PointF>listFingerPositions = new HashMap<>();
+    private final HashMap<Integer, Circle> listFingerCircles = new HashMap<>();
+    private final Random random = new Random();
+
+    private Handler handler = new Handler();
+
+    private boolean winnerSelected = false;
 
     public FingerPicker(Context context) {
         super(context);
     }
 
+    private void selectWinner(){
+        Log.d("FingerPicker", "Select Winner Aufruf");
+        if(listFingerCircles.size() >= 2){
+            Log.d("FingerPicker", "Select Winner if Aufruf erfüllt");
+            int randomIndex = random.nextInt(listFingerCircles.size()-1);
+            Circle winnerCircle = listFingerCircles.get(randomIndex);
+
+            setBackgroundColor(winnerCircle.getColor());
+
+            Toast.makeText(getContext(), "Gewinner: Kreis " + randomIndex, Toast.LENGTH_SHORT).show();
+            winnerSelected = true;
+        }
+    }
+
     protected void onDraw(Canvas canvas) {
-
-
-        for(PointF fingerPosition : listFingerPositions.values()){
-            paint.setColor(Color.RED);
-            canvas.drawCircle(fingerPosition.x,fingerPosition.y, 200f, paint);
+        for (Circle circle : listFingerCircles.values()) {
+            paint.setColor(circle.getColor());
+            canvas.drawCircle(circle.getX(), circle.getY(), 200f, paint);
         }
         invalidate();
     }
 
-
     @SuppressLint("ClickableViewAccessibility")
     public boolean onTouchEvent(MotionEvent event) {
-
-        //Index der Arrays in der die Fingerposition (x und y) gespeichert sind
         int pointerIndex = event.getActionIndex();
-        //Eindeutige Kennung die einem Finger zugeordnet wird
         int pointerId = event.getPointerId(pointerIndex);
 
         switch (event.getActionMasked()) {
             case MotionEvent.ACTION_DOWN:
             case MotionEvent.ACTION_POINTER_DOWN:
+                Log.d("FingerPicker", "ACTION_POINTER_DOWN");
                 PointF fingerPosition = new PointF(event.getX(pointerIndex), event.getY(pointerIndex));
-                listFingerPositions.put(pointerId, fingerPosition);
+                int randomColor = generateRandomColor();
+                Circle circle = new Circle(fingerPosition.x, fingerPosition.y, randomColor);
+                listFingerCircles.put(pointerId, circle);
                 invalidate();
+
+                handler.postDelayed(new Runnable(){
+                    public void run(){
+                        Log.d("FingerPicker", "run Aufruf");
+                        if(winnerSelected == false){
+                            Log.d("FingerPicker", "run if erfüllt");
+                            Toast.makeText(getContext(), "Delaying ...", Toast.LENGTH_SHORT).show();
+                            selectWinner();
+                        }
+                    }
+                }, 3000);
+
                 break;
             case MotionEvent.ACTION_MOVE:
-                for(int i=0; i<event.getPointerCount(); i++){
+                for (int i = 0; i < event.getPointerCount(); i++) {
                     int id = event.getPointerId(i);
-                    fingerPosition = listFingerPositions.get(id);
-                    if(fingerPosition != null){
-                        fingerPosition.x = event.getX(i);
-                        fingerPosition.y = event.getY(i);
+                    Circle circle1 = listFingerCircles.get(id);
+                    if (circle1 != null) {
+                        circle1.setX(event.getX(i));
+                        circle1.setY(event.getY(i));
                     }
                 }
-                //Log.i(TAG, "ACTION_MOVE");
                 invalidate();
                 break;
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_POINTER_UP:
-                Log.i(TAG, "ACTION_UP");
-                listFingerPositions.remove(pointerId);
+                Log.d("FingerPicker", "ACTION_POINTER_UP");
+                listFingerCircles.remove(pointerId);
+
+                if(listFingerCircles.size() > 1){
+                    winnerSelected = false;
+                    Log.d("FingerPicker", "winnerSelected = false gesetzt");
+                }
+
                 invalidate();
                 break;
         }
         return true;
+    }
 
+    private int generateRandomColor() {
+        int red = random.nextInt(220);
+        int green = random.nextInt(220);
+        int blue = random.nextInt(220);
+        return Color.rgb(red, green, blue);
+    }
+
+    private static class Circle {
+        private float x;
+        private float y;
+        private int color;
+
+        public Circle(float x, float y, int color) {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+        }
+
+        public float getX() {
+            return x;
+        }
+
+        public void setX(float x) {
+            this.x = x;
+        }
+
+        public float getY() {
+            return y;
+        }
+
+        public void setY(float y) {
+            this.y = y;
+        }
+
+        public int getColor() {
+            return color;
+        }
     }
 }
